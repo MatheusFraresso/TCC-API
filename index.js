c0475da7980ab3b6e428ba228ab96db93636a1cf
MatheusFraresso <matheus-fraresso@matheus-laptop>
2023-03-02 03:16:45 -0300

    implements simple node api

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 require("dotenv").config()
 const express = require("express")
+const {
+  create,
+  read,
+  update,
+  remove,
+} = require("./src/controllers/Mongo/PacientController")
 
 const app = express()
 app.use(express.json())
@@ -8,3 +14,8 @@ var port = 3001
 app.listen(process.env.PORT || port, () => {
   console.log("Server is up and running on port " + port)
 })
+
+app.post("/pacient", (req, res) => create(req, res))
+app.get("/pacient/:id", (req, res) => read(req, res))
+app.patch("/pacient/:id", (req, res) => update(req, res))
+app.delete("/pacient/:id", (req, res) => remove(req, res))
