cdac88d7c721cb9b20e4df574493c29f1a8e3063
MatheusFraresso <matheus-fraresso@matheus-laptop>
2023-09-22 00:00:04 -0300

    Adds files

---
+++ routes/MedicRoutes.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const routes = express.Router()
+const controller = require("../controllers/MedicController")
+
+routes.use(authMiddleware)
+
+// SQL routes
+routes.post("/sql/create", controller.create)
+routes.post("/sql/read", controller.create)
+routes.post("/sql/update", controller.create)
+routes.post("/sql/delete", controller.create)
+routes.post("/sql/all", controller.create)
+
+// Mongo Routes
+routes.post("/mongo/create", controller.create)
+routes.post("/mongo/read", controller.create)
+routes.post("/mongo/update", controller.create)
+routes.post("/mongo/delete", controller.create)
+routes.post("/mongo/all", controller.create)
+
+module.exports = routes
---
+++ services/mongo/mongoose-models/ExamModel.js
@@ -0,0 +1,5 @@
+const mongoose = require("mongoose")
+
+const PatientSchema = new mongoose.Schema({})
+
+mongoose.model("Patient", PatientSchema)
